19520d60573df48fcd7bff5c6c7c381534228ef4
3igzag <36519350+3igzag@users.noreply.github.com>
2018-05-23 14:41:26 +0100

    bad alloc removed

--- src/crypto/slow-hash.cpp
+++ src/crypto/slow-hash.cpp
@@ -67,7 +67,8 @@ namespace Crypto {
 
   cn_context::~cn_context() {
     if (munmap(data, MAP_SIZE) != 0) {
-      throw bad_alloc();
+      std::terminate();
+      // throw bad_alloc();
     }
   }
 
